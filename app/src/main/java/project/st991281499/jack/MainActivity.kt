fd2bd6aceae2363b630c3feec586a40ce0d5229c
jackjozwik <56364185+jackjozwik@users.noreply.github.com>
2021-11-16 18:17:22 -0500

    Untrack files in .gitignore

---
+++ app/src/main/java/project/st991281499/jack/MainActivity.kt
@@ -0,0 +1,11 @@
+package project.st991281499.jack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
