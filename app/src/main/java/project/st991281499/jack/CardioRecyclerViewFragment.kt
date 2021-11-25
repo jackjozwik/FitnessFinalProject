43822829871bdaf94f41e8721e0633a0a965b79d
Beattija <beattija@sheridancollege.ca>
2021-11-25 17:55:49 -0500

    Finished navigation with buttons for new data entry

--- app/src/main/java/project/st991281499/jack/CardioRecyclerViewFragment.kt
+++ app/src/main/java/project/st991281499/jack/CardioRecyclerViewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import project.st991281499.jack.databinding.FragmentCardioRecyclerViewBinding
 
 
@@ -16,8 +17,14 @@ class CardioRecyclerViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //view binding
         binding = FragmentCardioRecyclerViewBinding.inflate(layoutInflater)
 
+        //button nav to entry page
+        binding.cardioAddButton.setOnClickListener{
+            var action = CardioRecyclerViewFragmentDirections.actionCardioRecyclerViewFragmentToCardioEntryFragment()
+            findNavController().navigate(action)
+        }
         return (binding.root)
     }
 }
--- app/src/main/java/project/st991281499/jack/StrengthRecyclerViewFragment.kt
+++ app/src/main/java/project/st991281499/jack/StrengthRecyclerViewFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import project.st991281499.jack.databinding.FragmentStrengthRecyclerViewBinding
 
 class StrengthRecyclerViewFragment : Fragment() {
 
+    private lateinit var binding: FragmentStrengthRecyclerViewBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_strength_recycler_view, container, false)
+
+        //view binding
+        binding = FragmentStrengthRecyclerViewBinding.inflate(layoutInflater)
+
+        //button nav to entry page
+        binding.strAddButton.setOnClickListener{
+            var action = StrengthRecyclerViewFragmentDirections.actionStrengthRecyclerViewFragmentToStrengthEntryFragment()
+            findNavController().navigate(action)
+        }
+
+
+        return (binding.root)
     }
 
 }
